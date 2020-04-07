0674ebc66e7986b8853b7148e17232d7a838e7ef
Daniel Truong <ddtruong06@gmail.com>
2020-04-07 00:38:19 -0700

    Created blank Twilio file

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask_cors import CORS
 from twitter.botscript import bot
 from data import data
 from news import news
+from twilio import twilio
 
 app = Flask(__name__)
 
 app.register_blueprint(bot, url_prefix="/bot")
 app.register_blueprint(data, url_prefix="/data")
 app.register_blueprint(news, url_prefix="/news")
+app.register_blueprint(twilio, url_prefix="/twilio")
 
 CORS(app)
 
